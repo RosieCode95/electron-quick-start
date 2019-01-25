795765d0f3eefdfb6fdd6e4880ae8d08d2d6cc92
RosieCode95 <rosiecode@outlook.com>
2019-01-25 04:48:40 +0000

    electron bug

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, systemPreferences} = require('electron')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -21,6 +21,14 @@ function createWindow () {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     mainWindow = null
+  })
+
+  mainWindow.webContents.once('dom-ready', () => {
+    const color = systemPreferences.getAccentColor()
+    console.log(color);
+    mainWindow.webContents.send('dom-color', color)
+    systemPreferences.on('accent-color-changed', (event, color) => { console.log(color); mainWindow.webContents.send('dom-color', color) })
+    if (process.env.NODE_ENV === 'development') { mainWindow.webContents.openDevTools() }
   })
 }
 
